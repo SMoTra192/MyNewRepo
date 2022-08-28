2b1d12c5f67d5da9236b276a45d17b71fdbc964d
SMoTra192 <m.golub2002@gmail.com>
2022-08-28 20:09:14 +0300

    Merge branch 'main' of https://github.com/SMoTra192/MyNewRepo

---
+++ Assets/Script/Light.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     print(message:"Lightning");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    print(message:"Lightning");    
+    }
+}
